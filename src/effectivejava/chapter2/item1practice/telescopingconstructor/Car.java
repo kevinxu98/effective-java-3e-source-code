70b1c7bc508660ef817c84f3e03c862b78b6f520
Kevin <kevinxu360@outlook.com>
2025-06-22 17:42:23 -0700

    ...

---
+++ src/effectivejava/chapter2/item1practice/telescopingconstructor/Car.java
@@ -0,0 +1,33 @@
+package effectivejava.chapter2.item1practice.telescopingconstructor;
+
+
+public class Car {
+  private final String color;
+  private final String make;
+  private final int year;
+  private final int id;
+
+  public Car(String color) {
+    this(color, "");
+  }
+
+  public Car(String color, String make) {
+    this(color, make, 0);
+  }
+
+  public Car(String color, String make, int year){
+    this(color, make, year,  0);
+  }
+
+  public Car(String color, String make, int year, int id) {
+    this.color = color;
+    this.make = make;
+    this.year = year;
+    this.id = id;
+  }
+
+  public static void main(String[] args){
+    Car car1 = new Car("black");
+    Car car2 = new Car("black", "tesla");
+  }
+}
